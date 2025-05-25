75b352ce7f06beaaf9e5bd893157491a3aef5b86
daniel-kav <dankavatha01@gmail.com>
2025-05-25 16:39:11 +0300

    feat: implement appointment creation signal to send confirmation email

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .tasks import send_appointment_confirmation
 
 # Create your models here.
+
+@receiver(post_save, sender=Appointment)
+def appointment_created(sender, instance, created, **kwargs):
+    if created:
+        # Send confirmation email asynchronously
+        send_appointment_confirmation.delay(instance.id)
